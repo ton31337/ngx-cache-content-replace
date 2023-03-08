6fca38aed4681ccc29139bd6d0896fa50ef8c900
Donatas Abraitis <donatas@opensourcerouting.org>
2023-03-08 13:55:09 +0200

    Refactor code a bit
    Move parseCache into separate method and its own structures.
    
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

--- main.go
+++ main.go
@@ -66,6 +66,15 @@ type FileHandler struct {
 	File *os.File
 }
 
+type CacheHandler struct {
+	Status      []byte
+	CacheFile   *FileHandler
+	CacheHeader *Header
+	CacheKey    []byte
+	CacheKeyLen uint16
+	Headers     textproto.MIMEHeader
+}
+
 func init() {
 	rootCmd.Flags().StringVar(&cacheFile, "cache-file", "", "The Nginx's an existing cache file")
 	rootCmd.Flags().StringVar(&dataFile, "data-file", "", "The data file to replace an existing cached body")
@@ -116,26 +125,72 @@ func openFile(file string) (*FileHandler, error) {
 	return fh, nil
 }
 
-func extractCacheBody() error {
+func parseCache() (*CacheHandler, error) {
+	ch := &CacheHandler{}
+
 	fCache, err := openFile(cacheFile)
+	if err != nil {
+		return nil, err
+	}
+
+	ch.CacheFile = fCache
+
+	h := new(Header)
+	if err := binary.Read(fCache.File, byteOrder, h); err != nil {
+		return nil, err
+	}
+
+	ch.CacheHeader = h
+
+	// Read cache key (= KEY: 0-http://blabla...)
+	cache_key_len := h.HeaderStart - uint16(HeaderSize) - 1
+	var cache_key []byte = make([]byte, cache_key_len)
+	fCache.File.Seek(int64(HeaderSize), io.SeekStart)
+	_, err = fCache.File.Read(cache_key[:])
+	if err != nil {
+		return nil, err
+	}
+
+	ch.CacheKey = cache_key
+	ch.CacheKeyLen = cache_key_len
+
+	// Read headers, and status line from the original cache file
+	fCache.File.Seek(int64(h.HeaderStart), io.SeekStart)
+	htr := bufio.NewReader(io.MultiReader(
+		io.LimitReader(fCache.File, int64(h.BodyStart-h.HeaderStart)),
+		strings.NewReader("\r\n"),
+	))
+	status, _ := htr.ReadBytes('\n')
+	headers, err := textproto.NewReader(htr).ReadMIMEHeader()
+	if err != nil {
+		return nil, err
+	}
+
+	ch.Status = status
+	ch.Headers = headers
+
+	return ch, nil
+}
+
+func extractCacheBody() error {
+	ch, err := parseCache()
 	if err != nil {
 		return err
 	}
-	defer fCache.File.Close()
+	defer ch.CacheFile.File.Close()
+	headers := ch.Headers
 
 	out, err := os.Create(extractToFile)
 	if err != nil {
 		return err
 	}
 
-	h := new(Header)
-	if err := binary.Read(fCache.File, byteOrder, h); err != nil {
-		return err
-	}
+	fmt.Printf("Content-Type: %s\n", headers.Get("Content-Type"))
+	fmt.Printf("Content-Length: %s\n", headers.Get("Content-Length"))
 
-	fCache.File.Seek(int64(h.BodyStart), io.SeekStart)
+	ch.CacheFile.File.Seek(int64(ch.CacheHeader.BodyStart), io.SeekStart)
 
-	io.Copy(out, fCache.File)
+	io.Copy(out, ch.CacheFile.File)
 
 	return nil
 }
@@ -149,39 +204,14 @@ func updateCacheBody() error {
 	}
 	defer fData.File.Close()
 
-	fCache, err := openFile(cacheFile)
+	ch, err := parseCache()
 	if err != nil {
 		return err
 	}
-	defer fCache.File.Close()
+	defer ch.CacheFile.File.Close()
+	headers := ch.Headers
 
 	out, err := os.Create(tmpCacheFile)
-	if err != nil {
-		return err
-	}
-
-	h := new(Header)
-	if err := binary.Read(fCache.File, byteOrder, h); err != nil {
-		return err
-	}
-
-	// Read cache key (= KEY: 0-http://blabla...)
-	cache_key_len := h.HeaderStart - uint16(HeaderSize) - 1
-	var cache_key []byte = make([]byte, cache_key_len)
-	fCache.File.Seek(int64(HeaderSize), io.SeekStart)
-	_, err = fCache.File.Read(cache_key[:])
-	if err != nil {
-		return err
-	}
-
-	// Read headers, and status line from the original cache file
-	fCache.File.Seek(int64(h.HeaderStart), io.SeekStart)
-	htr := bufio.NewReader(io.MultiReader(
-		io.LimitReader(fCache.File, int64(h.BodyStart-h.HeaderStart)),
-		strings.NewReader("\r\n"),
-	))
-	status, _ := htr.ReadBytes('\n')
-	headers, err := textproto.NewReader(htr).ReadMIMEHeader()
 	if err != nil {
 		return err
 	}
@@ -193,35 +223,35 @@ func updateCacheBody() error {
 	}
 
 	// Save original data
-	fCache.File.Seek(int64(h.BodyStart), io.SeekStart)
+	ch.CacheFile.File.Seek(int64(ch.CacheHeader.BodyStart), io.SeekStart)
 	buff := bytes.NewBuffer(make([]byte, 0, 8192))
 	headerToBytes(buff, headers)
 	header_length := len(buff.Bytes())
 
 	// Change header/body start positions
-	h.HeaderStart = uint16(HeaderSize) + uint16(binary.Size(cache_key)) + 1
-	h.BodyStart = h.HeaderStart + uint16(header_length) + uint16(len(headers)) + 2
+	ch.CacheHeader.HeaderStart = uint16(HeaderSize) + uint16(binary.Size(ch.CacheKey)) + 1
+	ch.CacheHeader.BodyStart = ch.CacheHeader.HeaderStart + uint16(header_length) + uint16(len(headers)) + 2
 
-	fmt.Printf("Cache key length: %d\n", cache_key_len)
-	fmt.Printf("Cache key: %s\n", cache_key[len(cache_key_head):])
+	fmt.Printf("Cache key length: %d\n", ch.CacheKeyLen)
+	fmt.Printf("Cache key: %s\n", ch.CacheKey[len(cache_key_head):])
 	fmt.Printf("Headers length: %d\n", header_length)
-	fmt.Printf("Headers start: %d\n", h.HeaderStart)
+	fmt.Printf("Headers start: %d\n", ch.CacheHeader.HeaderStart)
 	fmt.Printf("Headers: %s\n", headers)
-	fmt.Printf("Body start: %d\n", h.BodyStart)
+	fmt.Printf("Body start: %d\n", ch.CacheHeader.BodyStart)
 
 	// Write generic cache file header
-	binary.Write(out, byteOrder, h)
+	binary.Write(out, byteOrder, ch.CacheHeader)
 	binary.Write(out, byteOrder, []byte{'\000', '\000', '\000', '\000'})
-	binary.Write(out, byteOrder, cache_key)
+	binary.Write(out, byteOrder, ch.CacheKey)
 	binary.Write(out, byteOrder, []byte{'\n'})
-	binary.Write(out, byteOrder, []byte(status))
+	binary.Write(out, byteOrder, []byte(ch.Status))
 
 	// Write headers
 	binary.Write(out, byteOrder, buff.Bytes())
 	binary.Write(out, byteOrder, []byte{'\r', '\n', '\r', '\n'})
 
 	// Write body
-	out.Seek(int64(h.BodyStart), io.SeekStart)
+	out.Seek(int64(ch.CacheHeader.BodyStart), io.SeekStart)
 	io.Copy(out, fData.File)
 
 	if !dryRun {
@@ -232,12 +262,12 @@ func updateCacheBody() error {
 			return err
 		}
 
-		err = os.Chmod(cacheFile, fs.FileMode(fCache.Info.Mode()))
+		err = os.Chmod(cacheFile, fs.FileMode(ch.CacheFile.Info.Mode()))
 		if err != nil {
 			return err
 		}
 
-		err = os.Chown(cacheFile, int(fCache.Stat.Uid), int(fCache.Stat.Gid))
+		err = os.Chown(cacheFile, int(ch.CacheFile.Stat.Uid), int(ch.CacheFile.Stat.Gid))
 		if err != nil {
 			return err
 		}
